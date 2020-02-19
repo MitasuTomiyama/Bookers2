47d725a273266803dd66d5bdfd97c411f25a15c2
MitasuTomiyama <m.tomiyama52@gmail.com>
2020-02-19 16:01:56 +0900

    [Add]Home-controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,17 @@
 class UsersController < ApplicationController
+  
+   before_action :authenticate_user!
+
   def index
   end
 
   def show
-    @book = Book.new
+    @user = User.find(params[:id])
     @book = Book.all
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
@@ -15,4 +19,6 @@ class UsersController < ApplicationController
 
   def top
   end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  root 'users#top'
+  get 'home/show'
+  get 'home/index'
+  get 'show/index'
+  root 'home#index'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
