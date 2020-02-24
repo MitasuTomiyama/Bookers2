cab5eaafc9735530975152e1087adba080c98a91
MitasuTomiyama <m.tomiyama52@gmail.com>
2020-02-24 17:02:59 +0900

    [Add]book編集ページShow/Back,link

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,8 +22,8 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book)
       flash[:notice] = "You have created book successfully"
+      redirect_to book_path(@book)
     else
       @user = current_user
       @books = Book.all
@@ -41,8 +41,8 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
     if
       book.update(book_params)
-      redirect_to book_path(params[:id])
       flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(params[:id])
     else
       @book = book
       render action: :edit
